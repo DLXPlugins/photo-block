8ad96eaf2069252716e47a8b256ee0b0aec0e4d6
Ronald Huereca <ronald@mediaron.com>
2025-05-05 03:31:42 -0500

    Adding dist files.

---
+++ dist/dlx-filepond.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5e9b559bc20d9ecc29bc');
---
+++ dist/dlx-pb-admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '2bb819c6ead770761b54');
