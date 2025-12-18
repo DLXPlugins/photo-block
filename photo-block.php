9c6b10ee4a1319b9bfd51c95255f4b5222981400
Ronald Huereca <ronald@mediaron.com>
2025-12-18 12:25:22 -0600

    Doing prod build.

--- photo-block.php
+++ photo-block.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://dlxplugins.com/plugins/photo-block/
  * Description: An easy to use, but comprehensive photo block for WordPress
  * Author: DLX Plugins
- * Version: 1.6.0
+ * Version: 1.6.1
  * Requires at least: 6.5
  * Requires PHP: 7.2
  * Author URI: https://dlxplugins.com/plugins/photo-block/
@@ -21,8 +21,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-define( 'DLX_PHOTO_BLOCK_VERSION', '1.6.0' );
-define( 'DLX_PHOTO_BLOCK_CACHE_VERSION', '1.0.12' ); // For cache busting global styles.
+define( 'DLX_PHOTO_BLOCK_VERSION', '1.6.1' );
+define( 'DLX_PHOTO_BLOCK_CACHE_VERSION', '1.1.0' ); // For cache busting global styles.
 define( 'DLX_PHOTO_BLOCK_FILE', __FILE__ );
 
 // Support for site-level autoloading.
