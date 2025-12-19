03e5622964c0c7ae5485bff7b5f0a5b0048c504f
Ronald Huereca <ronald@mediaron.com>
2025-12-18 20:28:29 -0600

    Version bump.

--- photo-block.php
+++ photo-block.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://dlxplugins.com/plugins/photo-block/
  * Description: An easy to use, but comprehensive photo block for WordPress
  * Author: DLX Plugins
- * Version: 1.7.4
+ * Version: 1.7.5
  * Requires at least: 6.5
  * Requires PHP: 7.2
  * Author URI: https://dlxplugins.com/plugins/photo-block/
