ef9be31babe9a00f903f4e9d3ee212dbf2339062
Ronald Huereca <ronald@mediaron.com>
2025-12-21 03:15:31 -0600

    Fixing ScreenshotOne API return result.

--- dist/dlx-pb-admin.asset.php
+++ dist/dlx-pb-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'af0a089869d438e32973');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '6696bcb2130450a540fb');
